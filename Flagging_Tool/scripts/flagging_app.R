29756d073f7622d78ca9d6f17be1e5e693d5d328
lclagorga <161762033+lclagorga@users.noreply.github.com>
2025-05-13 16:16:19 -0400

    Update flagging_app.R

--- Flagging_Tool/scripts/flagging_app.R
+++ Flagging_Tool/scripts/flagging_app.R
@@ -12,8 +12,11 @@ library(RColorBrewer)
 
 #arranging & laying out
 ui <- fluidPage(
-  theme = bs_theme(version = 5, bootswatch = "morph"), #journal, sketchy, minty, morph
-  titlePanel("flag on the play"),
+  theme =  bslib::bs_theme(version = 5, bootswatch = "morph") , #journal, sketchy, minty
+  titlePanel( div(img(src = "https://serc.si.edu/sites/default/files/website-gen/si_logo-primary-white.webp", height = "50px"),
+             br(),br(),
+            h1("flag on the play"))),
+  
   #upload
   wellPanel(
     actionButton("edit_defaults", "Edit File Defaults", class = "btn-primary"),
@@ -54,6 +57,7 @@ ui <- fluidPage(
                                 wellPanel(
                                   h4("selected points"),
                                   actionButton("flag_selected", "flag selected points", class = "btn-warning"),
+                                  br(),
                                   actionButton("clear_selection", "clear selection", class = "btn-info"),
                                   br(), br(),
                                   DTOutput("selected_points_table")
@@ -75,35 +79,53 @@ server <- function(input, output, session) {
     export = NULL,
     params = NULL,       
     selected_points = NULL,
-    ranges_applied = FALSE
+    ranges_applied = FALSE,
+    window = 30,
+    sds = 3,
+    timestamp_column = 1,
+    sitename_column = 2,
+    timeformat  = "%Y-%m-%d %H:%M:%S"
   )
   
   #add default editing into a modal, to clean up the UI
   observeEvent(input$edit_defaults, {
-    #popup
     showModal(modalDialog(
       title = "Edit File Defaults",
       h5("Provide some info before uploading your file!"),
       numericInput("timestamp_column", "Timestamp Column Index:", 1, min = 1),
       numericInput("sitename_column", "Site Name Column Index:", 2, min = 1),
-      selectInput("timeformat", "Timestamp Format (strptime):", choices = c("%Y-%m-%d %H:%M:%S", "%m/%d/%Y  %H:%M", "%m/%d/%Y  %H:%M:%S %p")),
+      selectInput("timeformat", "Timestamp Format (strptime):", 
+                  choices = c("%Y-%m-%d %H:%M:%S", "%m/%d/%Y  %H:%M", "%m/%d/%Y  %H:%M:%S %p")),
       h5("Enter values to determine outliers!"),
-      p("if you want an aggressive smooth, increase the rolling average window & decrease the standard deviation cut off."),
-      numericInput("window", "rolling average window:", 30, min = 10),
-      numericInput("sds", "standard deviations:", 3, min = 2),
+      p("If you want an aggressive smooth, increase the rolling average window & decrease the standard deviation cut-off."),
+      numericInput("window", "Rolling average window:", 30, min = 10),
+      numericInput("sds", "Standard deviations:", 3, min = 2),
       footer = tagList(
-        modalButton("Accept Changes")
+        actionButton("confirm_defaults", "Accept Changes")  
       ),
       easyClose = TRUE
     ))
   })
+  
+  #store inputs
+  observeEvent(input$confirm_defaults, {
+    req(input$timestamp_column, input$sitename_column, input$timeformat, input$window, input$sds) 
+    values$timestamp_column <- input$timestamp_column
+    values$sitename_column <- input$sitename_column
+    values$timeformat <- input$timeformat
+    values$window <- input$window
+    values$sds <- input$sds
+    removeModal() 
+  })
+  
+  
 #accept file for flagging. can take .csv/.dat/.txt
   observeEvent(input$file, {
     req(input$file)
     values$ranges_applied <- FALSE
-    timestamp_column <- input$timestamp_column
-    sitename_column <- input$sitename_column
-    timeformat <- input$timeformat
+    timestamp_column <- values$timestamp_column
+    sitename_column <- values$sitename_column
+    timeformat <- values$timeformat
     #set view to the range selectors
     updateTabsetPanel(session, "tabs", selected = "ranges")
     # read and format data (should probably add a modal/popup for if a file isn't formatted correctly or just edit so you can select your timestamp columns + add a site name)
@@ -165,8 +187,8 @@ server <- function(input, output, session) {
   # go! (do the rolling clean and range flagging )
   observeEvent(input$apply_ranges, {
     req(values$data, values$params)
-    window <- input$window
-    sds <- input$sds
+    window <- values$window
+    sds <- values$sds
     df <- values$flagged
     for(p in values$params) {
       range_id <- paste0("slider_", p)
@@ -190,104 +212,54 @@ server <- function(input, output, session) {
   output$plot <- renderPlotly({
     req(values$flagged, input$plot_param, values$ranges_applied)
     req(input$tabs == "flagging")
-    
     param <- input$plot_param
     flag_col <- paste0(param, "_flag")
     
-    #separate traces for each flag type
+    #flag colors
+    flag_colors <- list(
+      "unflagged" = "#A9a9a9",
+      "r" = "#ee5f5b",
+      "o" = "#5bc0de",
+      "oow" = "#5cb85c",
+      "d" = "#f89406",
+      "q" = "purple3"
+    )
+   
+     add_flag_trace <- function(p, data, flag, flagtag, flag_col, param, color) {
+      flagged_data <- if (flagtag == "unflagged") {
+        data[is.na(data[[flag_col]]), ]
+      } else {
+        data[data[[flag_col]] == flagtag, ]
+      }
+      p <- add_trace(p, data = flagged_data, 
+                     key = ~RowID,
+                     x = ~TIMESTAMP, 
+                     y = as.formula(paste0("~`", param, "`")),
+                     type = "scatter", 
+                     mode = "markers",
+                     marker = list(color = color, size = 5),
+                     name = flag)
+      return(p)
+    }
+    
     p <- plot_ly(values$flagged, source = "plot")
-   # "skyblue3","red3","navy","green3","#937A62","purple3"
-    #unflagged points
-    unflagged <- values$flagged[is.na(values$flagged[[flag_col]])]
-    if(nrow(unflagged) > 0) {
-      p <- add_trace(p, data = unflagged, 
-                     key = ~RowID,
-                     x = ~TIMESTAMP, 
-                     y = as.formula(paste0("~`", param, "`")),
-                     type = "scatter", 
-                     mode = "markers",
-                     marker = list(color = "skyblue3", size = 5),
-                     name = "unflagged")
-    }
+    p <- add_flag_trace(p, values$flagged, "Unflagged","unflagged", flag_col, param, flag_colors$unflagged)
+    p <- add_flag_trace(p, values$flagged, "Range", "r", flag_col, param, flag_colors$r)
+    p <- add_flag_trace(p, values$flagged, "Outlier","o", flag_col, param, flag_colors$o)
+    p <- add_flag_trace(p, values$flagged, "Out-of-water","oow", flag_col, param, flag_colors$oow)
+    p <- add_flag_trace(p, values$flagged, "Dry","d", flag_col, param, flag_colors$d)
+    p <- add_flag_trace(p, values$flagged, "Questionable","q", flag_col, param, flag_colors$q)
     
-    #range flags
-    r_flagged <- values$flagged[values$flagged[[flag_col]] == "r"]
-    if(nrow(r_flagged) > 0) {
-      p <- add_trace(p, data = r_flagged, 
-                     key = ~RowID,
-                     x = ~TIMESTAMP, 
-                     y = as.formula(paste0("~`", param, "`")),
-                     type = "scatter", 
-                     mode = "markers",
-                     marker = list(color = "red3", size = 5),
-                     name = "range")
-    }
-    
-    #outlier flags
-    o_flagged <- values$flagged[values$flagged[[flag_col]] == "o"]
-    if(nrow(o_flagged) > 0) {
-      p <- add_trace(p, data = o_flagged, 
-                     key = ~RowID,
-                     x = ~TIMESTAMP, 
-                     y = as.formula(paste0("~`", param, "`")),
-                     type = "scatter", 
-                     mode = "markers",
-                     marker = list(color = "navy", size = 5),
-                     name = "outlier")
-    }
-    
-    #out-of-water flags
-    oow_flagged <- values$flagged[values$flagged[[flag_col]] == "oow"]
-    if(nrow(oow_flagged) > 0) {
-      p <- add_trace(p, data = oow_flagged, 
-                     key = ~RowID,
-                     x = ~TIMESTAMP, 
-                     y = as.formula(paste0("~`", param, "`")),
-                     type = "scatter", 
-                     mode = "markers",
-                     marker = list(color = "green3", size = 5),
-                     name = "out-of-water")
-    }
-    
-    #dry flags
-    d_flagged <- values$flagged[values$flagged[[flag_col]] == "d"]
-    if(nrow(d_flagged) > 0) {
-      p <- add_trace(p, data = d_flagged, 
-                     key = ~RowID,
-                     x = ~TIMESTAMP, 
-                     y = as.formula(paste0("~`", param, "`")),
-                     type = "scatter", 
-                     mode = "markers",
-                     marker = list(color = "#937A62", size = 5),
-                     name = "dry")
-    }
-    
-    #questionable flags
-    q_flagged <- values$flagged[values$flagged[[flag_col]] == "q"]
-    if(nrow(d_flagged) > 0) {
-      p <- add_trace(p, data = q_flagged, 
-                     key = ~RowID,
-                     x = ~TIMESTAMP, 
-                     y = as.formula(paste0("~`", param, "`")),
-                     type = "scatter", 
-                     mode = "markers",
-                     marker = list(color = "purple3", size = 5),
-                     name = "questionable")
-    }
-    
-    #format step
     p <- layout(p, 
                 xaxis = list(title = "Time"),
                 yaxis = list(title = param),
                 dragmode = "select")
     
-    #trying to fix the event register warning message but idk where to put this
     event_register(p, 'plotly_selected')
     
     return(p)
-    
-  })  
-  
+  })
+
   #selected data from plot
   observe({
     selected_data <- event_data("plotly_selected", source = "plot")
@@ -395,7 +367,7 @@ server <- function(input, output, session) {
       notes <- paste0(" File:", base, "\n Flagged On:", today(), 
                       "\n Flagged By:", input$user,
                       "\n Notes:", input$notes)
-      writeLines(notes, file)  # Use writeLines instead of fwrite
+      writeLines(notes, file)  
     }
   )
   
