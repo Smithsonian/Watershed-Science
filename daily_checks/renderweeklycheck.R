2898d04e50075d86858f50515223e963852a614e
lclagorga <lclagorga@gmail.com>
2025-06-11 10:30:32 -0400

    auto update

--- daily_checks/renderweeklycheck.R
+++ daily_checks/renderweeklycheck.R
@@ -1,6 +1,6 @@
 ##this script knits the rmd
 # Set Pandoc path
 Sys.setenv(RSTUDIO_PANDOC = "C:/Program Files (x86)/pandoc-3.7.0.1")
-setwd("C:/Users/LaGorgaL.S/Documents/WS_Checks")
+setwd("C:/Users/LaGorgaL.S/Documents/Watershed-Science/daily_checks")
 library(rmarkdown)
-render("C:/Users/LaGorgaL.S/Documents/WS_Checks/anything_WEIRd.Rmd")
+render("C:/Users/LaGorgaL.S/Documents/Watershed-Science/daily_checks/anything_WEIRd.Rmd")
